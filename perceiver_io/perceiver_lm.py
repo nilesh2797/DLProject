2df32d75eb98302f7772653d318641ac141ba51b
nilesh2797 <nileshgupta2797@gmail.com>
2021-11-22 17:00:15 -0600

    update

---
+++ perceiver_io/perceiver_lm.py
@@ -0,0 +1,139 @@
+from typing import Optional
+import torch
+from torch import nn
+import pickle
+from perceiver_io import PerceiverEncoder, PerceiverDecoder, PerceiverIO
+
+class PerceiverLM(nn.Module):
+    """Encoder-decoder based language model."""
+    def __init__(
+        self,
+        vocab_size: int,
+        max_seq_len: int,
+        embedding_dim: int,
+        num_latents: int = 256,
+        latent_dim: int = 1280,
+        qk_out_dim = 8*32,
+        v_out_dim = None,
+        num_self_attn_heads=8,
+        num_cross_attn_heads=8,
+        num_decoder_attn_heads=8,
+        self_attn_widening_factor=1,
+        cross_attn_widening_factor=1,
+        num_blocks=1,
+        num_self_attn_per_block=12,
+        dropout: float = 0.0,
+        lm = True
+    ):
+        super().__init__()
+        self.lm = lm
+        self.token_embedding = nn.Embedding(vocab_size, embedding_dim)
+        self.position_embedding = nn.Embedding(max_seq_len, embedding_dim)
+        self.query_embedding = nn.Embedding(max_seq_len, embedding_dim)
+        self.decoder_token_bias = nn.Parameter(torch.randn(vocab_size))
+        if v_out_dim is None: v_out_dim = latent_dim
+        encoder = PerceiverEncoder(
+            num_latents=num_latents,
+            latent_dim=latent_dim,
+            input_dim=embedding_dim,
+            qk_out_dim=qk_out_dim,
+            v_out_dim=v_out_dim,
+            num_self_attn_per_block=num_self_attn_per_block,
+            num_blocks=num_blocks,
+            num_self_attn_heads=num_self_attn_heads,
+            num_cross_attn_heads=num_cross_attn_heads,
+            cross_attn_widening_factor=cross_attn_widening_factor,
+            self_attn_widening_factor=self_attn_widening_factor,
+            dropout=dropout,
+        )
+        decoder = PerceiverDecoder(
+            latent_dim=latent_dim,
+            query_dim=embedding_dim,
+            qk_out_dim=qk_out_dim,
+            v_out_dim=embedding_dim,
+            num_heads=num_decoder_attn_heads,
+            widening_factor=cross_attn_widening_factor,
+            projection_dim=None
+        )
+        self.perceiver = PerceiverIO(encoder, decoder)
+
+    def forward(
+        self,
+        inputs: torch.Tensor,
+        mask: Optional[torch.Tensor] = None
+    ):
+        """
+        Args:
+            inputs: Tensor of token ids.
+            mask: Token mask. Mask values selected in [0, 1]. Defaults to None.
+        Returns:
+            Tensor of shape (batch_size, seq_len, vocab_size).
+        """
+        seq_len = inputs.size(1)
+        token_embeddings = self.token_embedding(inputs)
+        positions_ids = torch.arange(seq_len, device=inputs.device).view(1, -1)
+        position_embeddings = self.position_embedding(positions_ids)
+        embeddings = token_embeddings + position_embeddings
+        query_embeddings = self.query_embedding(positions_ids)
+        outputs = self.perceiver(
+            inputs=embeddings,
+            query=query_embeddings,
+            input_mask=mask,
+            query_mask=mask
+        )
+        if self.lm:
+            logits = torch.matmul(outputs, self.token_embedding.weight.T) + self.decoder_token_bias
+            return logits
+        return outputs
+    
+    def load_pretrained(self, fname, num_layers=26):
+        params = None
+        with open(fname, "rb") as f:
+            params = pickle.loads(f.read())
+        state_dict = {}
+        model_enc_base = 'perceiver.encoder.'
+        params_enc_base = 'perceiver_encoder/~/'
+
+        state_dict['token_embedding.weight'] = params['embed']['embeddings']
+        state_dict['decoder_token_bias'] = params['embedding_decoder']['bias']
+        state_dict['position_embedding.weight'] = params['trainable_position_encoding']['pos_embs']
+        state_dict['query_embedding.weight'] = params['basic_decoder/~/trainable_position_encoding']['pos_embs']
+        state_dict[f'{model_enc_base}latents'] = params[f'{params_enc_base}trainable_position_encoding']['pos_embs']
+
+        def copy_attention_params(state_dict, model_base, params_base):
+            state_dict[f'{model_base}attention.q.weight'] = params[f'{params_base}attention/linear']['w'].T
+            state_dict[f'{model_base}attention.q.bias'] = params[f'{params_base}attention/linear']['b']
+            state_dict[f'{model_base}attention.k.weight'] = params[f'{params_base}attention/linear_1']['w'].T
+            state_dict[f'{model_base}attention.k.bias'] = params[f'{params_base}attention/linear_1']['b']
+            state_dict[f'{model_base}attention.v.weight'] = params[f'{params_base}attention/linear_2']['w'].T
+            state_dict[f'{model_base}attention.v.bias'] = params[f'{params_base}attention/linear_2']['b']
+            state_dict[f'{model_base}attention.projection.weight'] = params[f'{params_base}attention/linear_3']['w'].T
+            state_dict[f'{model_base}attention.projection.bias'] = params[f'{params_base}attention/linear_3']['b']
+
+            if 'self_attention' in params_base:
+                state_dict[f'{model_base}layer_norm.weight'] = params[f'{params_base}layer_norm']['scale']
+                state_dict[f'{model_base}layer_norm.bias'] = params[f'{params_base}layer_norm']['offset']
+                state_dict[f'{model_base}qkv_layer_norm.weight'] = params[f'{params_base}layer_norm_1']['scale']
+                state_dict[f'{model_base}qkv_layer_norm.bias'] = params[f'{params_base}layer_norm_1']['offset']
+            else:
+                state_dict[f'{model_base}q_layer_norm.weight'] = params[f'{params_base}layer_norm']['scale']
+                state_dict[f'{model_base}q_layer_norm.bias'] = params[f'{params_base}layer_norm']['offset']
+                state_dict[f'{model_base}kv_layer_norm.weight'] = params[f'{params_base}layer_norm_1']['scale']
+                state_dict[f'{model_base}kv_layer_norm.bias'] = params[f'{params_base}layer_norm_1']['offset']
+                state_dict[f'{model_base}qkv_layer_norm.weight'] = params[f'{params_base}layer_norm_2']['scale']
+                state_dict[f'{model_base}qkv_layer_norm.bias'] = params[f'{params_base}layer_norm_2']['offset']
+
+            state_dict[f'{model_base}mlp.mlp.0.weight'] = params[f'{params_base}mlp/linear']['w'].T
+            state_dict[f'{model_base}mlp.mlp.0.bias'] = params[f'{params_base}mlp/linear']['b']
+            state_dict[f'{model_base}mlp.mlp.2.weight'] = params[f'{params_base}mlp/linear_1']['w'].T
+            state_dict[f'{model_base}mlp.mlp.2.bias'] = params[f'{params_base}mlp/linear_1']['b']
+            return state_dict
+
+        state_dict = copy_attention_params(state_dict, f'{model_enc_base}cross_attn.', f'{params_enc_base}cross_attention/')
+        state_dict = copy_attention_params(state_dict, f'perceiver.decoder.cross_attention.', f'basic_decoder/cross_attention/')
+
+        for i in range(num_layers):
+            state_dict = copy_attention_params(state_dict, f'{model_enc_base}self_attention_block.{i}.', f'{params_enc_base}self_attention{"_%d"%i if i else ""}/')
+
+        state_dict = {k: torch.tensor(v) for k,v in state_dict.items()}
+        self.load_state_dict(state_dict)
---
+++ utils.py
@@ -0,0 +1,113 @@
+
+import os, sys, math, random, time
+import torch
+import torch.nn as nn
+import numpy as np
+import pandas as pd
+import pickle as pkl
+import scipy.sparse as sp
+from typing import List, Dict, Tuple, Iterable, Type, Union, Callable
+from tqdm import tqdm
+import xclib.evaluation.xc_metrics as xc_metrics
+import xclib.data.data_utils as data_utils
+
+def ToD(batch, device):
+    if isinstance(batch, torch.Tensor):
+        batch = batch.to(device)
+    if isinstance(batch, Dict):
+        for outkey in batch:
+            if isinstance(batch[outkey], torch.Tensor):
+                batch[outkey] = batch[outkey].to(device)
+            if isinstance(batch[outkey], Dict):
+                for inkey in batch[outkey]:
+                    if isinstance(batch[outkey][inkey], torch.Tensor):
+                        batch[outkey][inkey] = batch[outkey][inkey].to(device)
+    return batch
+
+def csr_to_pad_tensor(spmat, pad):
+    inds_tensor = torch.LongTensor(spmat.indices)
+    data_tensor = torch.LongTensor(spmat.data)
+    return {'inds': torch.nn.utils.rnn.pad_sequence([inds_tensor[spmat.indptr[i]:spmat.indptr[i+1]] for i in range(spmat.shape[0])], batch_first=True, padding_value=pad),
+           'vals': torch.nn.utils.rnn.pad_sequence([data_tensor[spmat.indptr[i]:spmat.indptr[i+1]] for i in range(spmat.shape[0])], batch_first=True, padding_value=0.0)}
+
+def read_sparse_mat(filename, use_xclib=True):
+    if use_xclib:
+        return xclib.data.data_utils.read_sparse_file(filename)
+    else:
+        with open(filename) as f:
+            nr, nc = map(int, f.readline().split(' '))
+            data = []; indices = []; indptr = [0]
+            for line in tqdm(f):
+                if len(line) > 1:
+                    row = [x.split(':') for x in line.split()]
+                    tempindices, tempdata = list(zip(*row))
+                    indices.extend(list(map(int, tempindices)))
+                    data.extend(list(map(float, tempdata)))
+                    indptr.append(indptr[-1]+len(tempdata))
+                else:
+                    indptr.append(indptr[-1])
+            score_mat = sp.csr_matrix((data, indices, indptr), (nr, nc))
+            del data, indices, indptr
+            return score_mat
+        
+from xclib.utils.sparse import rank as sp_rank
+
+def _topk(rank_mat, K, inplace=False):
+    topk_mat = rank_mat if inplace else rank_mat.copy()
+    topk_mat.data[topk_mat.data > K] = 0
+    topk_mat.eliminate_zeros()
+    return topk_mat
+
+def Recall(rank_intrsxn_mat, true_mat, K=[1,3,5,10,20,50,100]):
+    K = sorted(K, reverse=True)
+    topk_intrsxn_mat = rank_intrsxn_mat.copy()
+    res = {}
+    for k in K:
+        topk_intrsxn_mat = _topk(topk_intrsxn_mat, k, inplace=True)
+        res[k] = (topk_intrsxn_mat.getnnz(1)/true_mat.getnnz(1)).mean()*100.0
+    return res
+
+def MRR(rank_intrsxn_mat, true_mat, K=[1,3,5,10,20,50,100]):
+    K = sorted(K, reverse=True)
+    topk_intrsxn_mat = _topk(rank_intrsxn_mat, K[0], inplace=True)
+    rr_topk_intrsxn_mat = topk_intrsxn_mat.copy()
+    rr_topk_intrsxn_mat.data = 1/rr_topk_intrsxn_mat.data
+    max_rr = rr_topk_intrsxn_mat.max(axis=1).toarray().ravel()
+    res = {}
+    for k in K:
+        max_rr[max_rr < 1/k] = 0.0
+        res[k] = max_rr.mean()*100
+    return res
+
+def XCMetrics(score_mat, X_Y, inv_prop, disp = True, fname = None, method = 'Method'): 
+    X_Y = X_Y.tocsr().astype(np.bool_)
+    acc = xc_metrics.Metrics(X_Y, inv_prop)
+    xc_eval_metrics = np.array(acc.eval(score_mat, 5))*100
+    xc_eval_metrics = pd.DataFrame(xc_eval_metrics)
+    
+    if inv_prop is None : xc_eval_metrics.index = ['P', 'nDCG']
+    else : xc_eval_metrics.index = ['P', 'nDCG', 'PSP', 'PSnDCG']    
+    xc_eval_metrics.columns = [(i+1) for i in range(5)]
+    
+    rank_mat = sp_rank(score_mat)
+    intrsxn_mat = rank_mat.multiply(X_Y)
+    ret_eval_metrics = pd.DataFrame({'R': Recall(intrsxn_mat, X_Y, K=[10, 20, 100]), 'MRR': MRR(intrsxn_mat, X_Y, K=[10])}).T
+    ret_eval_metrics = ret_eval_metrics.reindex(sorted(ret_eval_metrics.columns), axis=1)
+        
+    df1 = xc_eval_metrics[[1,3,5]].iloc[[0,1,2]].round(2).stack().to_frame().transpose()
+    df2 = ret_eval_metrics.iloc[[0,1]].round(2).stack().to_frame().transpose()
+
+    df = pd.concat([df1, df2], axis=1)
+    df.columns = [f'{col[0]}@{col[1]}' for col in df.columns.values]
+    df.index = [method]
+
+    if disp:
+        disp_df = df[['P@1', 'P@5', 'nDCG@1', 'nDCG@5', 'PSP@1', 'PSP@5', 'R@10', 'R@20', 'R@100', 'MRR@10']].round(2)
+        print(disp_df.to_csv(sep='\t', index=False))
+        print(disp_df.to_csv(sep=' ', index=False))
+    if fname is not None:
+        if os.path.splitext(fname)[-1] == '.json': df.to_json(fname)
+        elif os.path.splitext(fname)[-1] == '.csv': df.to_csv(fname)  
+        elif os.path.splitext(fname)[-1] == '.tsv': df.to_csv(fname, sep='\t')  
+        else: print(f'ERROR: File extension {os.path.splitext(fname)[-1]} in {fname} not supported')
+    return df
